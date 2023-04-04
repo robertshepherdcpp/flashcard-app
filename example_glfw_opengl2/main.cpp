3e29c1942dc2fa7004719cfc6bac984fb56c1c33
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2023-04-04 16:39:01 +0100

    try to remove the error

--- example_glfw_opengl2/main.cpp
+++ example_glfw_opengl2/main.cpp
@@ -91,7 +91,7 @@ int main(int, char**)
 
                 std::random_device dev;
                 std::mt19937 rng(dev());
-                std::uniform_int_distribution<std::mt19937::result_type> dist6(0, 1005); // distribution in range [1, 6]
+                std::uniform_int_distribution<std::mt19937::result_type> dist6(0, 1); // distribution in range [1, 6]
                 auto random = dist6(rng);
 
                 {
