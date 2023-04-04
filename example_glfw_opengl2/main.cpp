6927879f892a815950a53bafc28384d4ae8ef735
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2023-04-04 16:24:31 +0100

    start FlashCards++

--- example_glfw_opengl2/main.cpp
+++ example_glfw_opengl2/main.cpp
@@ -3,6 +3,7 @@
 #include "imgui_impl_opengl2.h"
 #include <stdio.h>
 #include <GLFW/glfw3.h>
+#include <fstream>
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
 #pragma comment(lib, "legacy_stdio_definitions")
@@ -48,96 +49,112 @@ int main(int, char**)
         ImGui_ImplOpenGL2_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
+        static bool window_open = true;
 
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
         {
-            static int button = 1;
+            ImGui::Begin("FlashCards++", &window_open);
 
-            ImGui::Begin("Pizary");
-            ImGui::BeginMenuBar();
-
-            if (button < 10 && button >= 0)
+            if (ImGui::Button("Test Me on flash cards"))
             {
-                if (
-                    ImGui::Button("Click me for how many pizzas"))
-                {
-                    button++;
-                }
-                else if (ImGui::Button("Click me to get rid of some pizzas"))
-                {
-                    --button;
-                }
+                /*Test on flash cards using a file*/
             }
-            else if(button >= 10)
+            if (ImGui::Button("Add another flash card"))
             {
-                if (ImGui::Button("Click me to decrease some pizzas!"))
-                {
-                    --button;
-                }
+                /*Add another flash card to the flash cards file*/
             }
-            else if (button <= 0)
+            if (ImGui::Button("Show me the flash cards"))
             {
-                ImGui::NewLine();
-                ImGui::Text("So your telling me you want -1 pizzas?\nThat can't be right!");
+                /*Open a window that shows all of the flash cards and opens new windows for new flash cards*/
             }
-            ImGui::NewLine();
-            ImGui::Text("You want %d pizzas", button);
-
-            ImGui::NewLine();
-
-            if (!exceeds_limit(button, 10))
+            if (ImGui::Button("Quit"))
             {
-                ImGui::Text("Hold on there buddy!\nYou have reached our maximum amount of pizzas!");
-            }
-            else
-            {
-                ImGui::Text("You can order %d more pizzas at most!", 10 - button);
+                /*Exit all windows*/
+                window_open = false;
             }
 
-            ImGui::NewLine();
-            if (ImGui::Button("Click here to reset"))
-            {
-                button = 1;
-            }
+            //if (button < 10 && button >= 0)
+            //{
+            //    if (
+            //        ImGui::Button("Click me for how many pizzas"))
+            //    {
+            //        button++;
+            //    }
+            //    else if (ImGui::Button("Click me to get rid of some pizzas"))
+            //    {
+            //        --button;
+            //    }
+            //}
+            //else if(button >= 10)
+            //{
+            //    if (ImGui::Button("Click me to decrease some pizzas!"))
+            //    {
+            //        --button;
+            //    }
+            //}
+            //else if (button <= 0)
+            //{
+            //    ImGui::NewLine();
+            //    ImGui::Text("So your telling me you want -1 pizzas?\nThat can't be right!");
+            //}
+            //ImGui::NewLine();
+            //ImGui::Text("You want %d pizzas", button);
 
-            ImGui::NewLine();
-            ImGui::Bullet();
-            ImGui::Text("Contact us on 07903873501");
+            //ImGui::NewLine();
 
-            ImGui::NewLine();
-            ImGui::Bullet();
-            ImGui::Text("Or contact us on email at rshepherdcpp@gmail.com");
+            //if (!exceeds_limit(button, 10))
+            //{
+            //    ImGui::Text("Hold on there buddy!\nYou have reached our maximum amount of pizzas!");
+            //}
+            //else
+            //{
+            //    ImGui::Text("You can order %d more pizzas at most!", 10 - button);
+            //}
 
-            static bool t_and_c = false;
+            //ImGui::NewLine();
+            //if (ImGui::Button("Click here to reset"))
+            //{
+            //    button = 1;
+            //}
 
-            ImGui::NewLine();
-            ImGui::Checkbox("Do you agree to the terms and conditions?", &t_and_c);
+            //ImGui::NewLine();
+            //ImGui::Bullet();
+            //ImGui::Text("Contact us on 07903873501");
 
-            if (t_and_c)
-            {
-                ImGui::Text("You don't know what you just agreed to.\n");
-                ImGui::Text("You have agreed to give me all of your JavaScript code!");
-            }
+            //ImGui::NewLine();
+            //ImGui::Bullet();
+            //ImGui::Text("Or contact us on email at rshepherdcpp@gmail.com");
 
-            ImGui::NewLine();
-            if (ImGui::ColorButton("colourful", clear_color))
-            {
-                ImGui::Text("You just chose to open a new window!\n");
-                ImGui::Text("Opening Now");
+            //static bool t_and_c = false;
 
-                randome_window = true;
-            }
+            //ImGui::NewLine();
+            //ImGui::Checkbox("Do you agree to the terms and conditions?", &t_and_c);
 
-            
+            //if (t_and_c)
+            //{
+            //    ImGui::Text("You don't know what you just agreed to.\n");
+            //    ImGui::Text("You have agreed to give me all of your JavaScript code!");
+            //}
 
-            ImGui::NewLine();
-            ImVec4 magical_colour = ImVec4(0.102f, 0.96f, 0.72f, 0.15f);
+            //ImGui::NewLine();
+            //if (ImGui::ColorButton("colourful", clear_color))
+            //{
+            //    ImGui::Text("You just chose to open a new window!\n");
+            //    ImGui::Text("Opening Now");
 
-            if (ImGui::ColorButton("Click here to stop", magical_colour))
-            {
-                break;
-            }
-            ImGui::Text("Click Here To Stop");
+            //    randome_window = true;
+            //}
+
+
+
+            //ImGui::NewLine();
+            //ImVec4 magical_colour = ImVec4(0.102f, 0.96f, 0.72f, 0.15f);
+
+            //if (ImGui::ColorButton("Click here to stop", magical_colour))
+            //{
+            //    break;
+            //}
+            //ImGui::Text("Click Here To Stop");
 
             ImGui::End();
         }
@@ -156,26 +173,26 @@ int main(int, char**)
             }
         }
 
-        // Rendering
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
+            // Rendering
+            ImGui::Render();
+            int display_w, display_h;
+            glfwGetFramebufferSize(window, &display_w, &display_h);
+            glViewport(0, 0, display_w, display_h);
+            glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+            glClear(GL_COLOR_BUFFER_BIT);
 
-        ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
-        glfwMakeContextCurrent(window);
-        glfwSwapBuffers(window);
+            ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+            glfwMakeContextCurrent(window);
+            glfwSwapBuffers(window);
+        }
+
+        // Cleanup
+        ImGui_ImplOpenGL2_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+
+        glfwDestroyWindow(window);
+        glfwTerminate();
+
+        return 0;
     }
-
-    // Cleanup
-    ImGui_ImplOpenGL2_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-
-    return 0;
-}
