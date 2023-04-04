ead06062d5a403db798fbcf53a172c573d7dd4ea
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2023-04-04 16:59:06 +0100

    make comment correct

--- example_glfw_opengl2/main.cpp
+++ example_glfw_opengl2/main.cpp
@@ -93,7 +93,7 @@ int main(int, char**)
 
                 std::random_device dev;
                 std::mt19937 rng(dev());
-                std::uniform_int_distribution<std::mt19937::result_type> dist6(0, vec.size()); // distribution in range [1, 6]
+                std::uniform_int_distribution<std::mt19937::result_type> dist6(0, vec.size()); // distribution in range [1, vec.size()]
                 auto random = dist6(rng);
                 to_test_flash_cards = true;
                 _question = vec[random].question;
